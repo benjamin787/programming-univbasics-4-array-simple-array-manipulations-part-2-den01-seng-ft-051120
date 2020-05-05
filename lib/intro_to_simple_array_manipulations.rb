e2991effd2fd5745de6a4999fbd8e38699c25a40
Benjamin Rowe <browe495@gmail.com>
2020-05-05 03:54:49 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(a1, a2)
+  a1.concat(a2)
+end 
+
+def using_insert(array, x)
+  array.insert(4, x)
+end 
+
+def using_uniq(array)
+  array.uniq 
+end 
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end 
+
+def using_delete_at(array, x)
+  array.delete_at(x)
+end 
